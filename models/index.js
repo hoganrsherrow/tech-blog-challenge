a2ca797dc041016944fb444e1c0ba343cf5dc4a0
hoganrsherrow <hogan.r.sherrow@vanderbilt.edu>
2022-09-18 16:39:48 -0500

    Added Post and Comment models and created Associations

--- models/index.js
+++ models/index.js
@@ -1,12 +1,30 @@
 const User = require('./User.js');
-
+const Post = require('./Post.js');
+const Comment = require('./Comment.js');
 
 // Create Associations here
-
-
-
-
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+});
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
 
 
 // export data
-module.exports = { User, Post, Comment }
+module.exports = { User, Post, Comment };
